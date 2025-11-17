daf06191715be05a18012c0850ec35930282aece
MiaSofie01 <miangaard@outlook.dk>
2025-11-17 12:54:26 +0100

    login-side

--- client/src/ui/App.tsx
+++ client/src/ui/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {createBrowserRouter, Outlet, RouterProvider} from "react-router";
+import Login from "./Pages/login/Login.tsx";
+import {Toaster} from "react-hot-toast";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+    return (
+        <>
+            <header><title>Gambling Site</title></header>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+            <RouterProvider router={createBrowserRouter([
+                {
+                    path: "/",
+                    element: <Outlet></Outlet>,
+                    children:[
+                        {
+                            path: "/login",
+                            element: <Login/>  // Forside = Login
+                        }
+                        // Flere routes kommer her senere (dashboard, games, osv.)
+                    ]
+                }
+            ])}
+            />
+            <Toaster position="top-center" reverseOrder={false}/>
+        </>
+    )
 }
-
-export default App
