e4285967d8ca6f03202bf39a342274e78238e5c6
Casper <casfro01@easv365.dk>
2025-11-17 13:06:33 +0100

    Service metode

---
+++ server/dataaccess/Entities/UserConst.cs
@@ -0,0 +1,15 @@
+﻿namespace DataAccess.Entities;
+
+public partial class User
+{
+    public User(string id, string username, string email, bool emailVerified, string passwordHash, string role, DateTime created)
+    {
+        Id = id;
+        UserName  = username;
+        Email = email;
+        EmailConfirmed = emailVerified;
+        PasswordHash = passwordHash;
+        Role = role;
+        Created = created;
+    }
+}
--- server/service/AuthService.cs
+++ server/service/AuthService.cs
@@ -1,20 +1,37 @@
-﻿using System.Security.Claims;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+using dataaccess;
+using DataAccess.Entities;
+using Microsoft.AspNetCore.Identity;
 using service.Abstractions;
 using service.Models.Request;
 using service.Models.Responses;
 
 namespace service;
 
-public class AuthService() : IAuthService
+public class AuthService(MyDbContext dbContext, PasswordHasher<User> passwordHasher) : IAuthService
 {
     public AuthUserInfo Authenticate(LoginRequest request)
     {
         throw new NotImplementedException();
     }
 
-    public Task<AuthUserInfo> Register(RegisterRequest request)
+    public async Task<AuthUserInfo> Register(RegisterRequest request)
     {
-        throw new NotImplementedException();
+        Validator.ValidateObject(request, new ValidationContext(request), true);
+        var user = new User(
+            id: Guid.NewGuid().ToString(),
+            email: request.Email,
+            emailVerified: false,
+            created:  DateTime.UtcNow,
+            username: request.UserName,
+            passwordHash: "",
+            role: "bruger"
+            );
+        user.PasswordHash = passwordHasher.HashPassword(user, request.Password);
+        dbContext.Users.Add(user);
+        await dbContext.SaveChangesAsync();
+        return new AuthUserInfo(user.Id, user.UserName,  user.Role);
     }
 
     public AuthUserInfo? GetUserInfo(ClaimsPrincipal principal)
