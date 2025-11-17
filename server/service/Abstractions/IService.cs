a68dc9925695721ba73a2dff5c6390a3a055ad67
Casper <casfro01@easv365.dk>
2025-11-17 10:25:38 +0100

    service abstraktion

---
+++ server/service/Abstractions/IService.cs
@@ -0,0 +1,11 @@
+﻿namespace service.Abstractions;
+
+public interface IService<TRes, in TCreate, in TUpdate>
+{
+    public Task<List<TRes>> Get();
+    public Task<TRes> Get(string id);
+
+    public Task<TRes> Create(TCreate request);
+    public Task<TRes> Update(TUpdate request);
+    public Task<TRes> Delete(string id);
+}
