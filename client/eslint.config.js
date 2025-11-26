ea43c0f5c28150f8d55ab71d195d6f02176be3eb
Lucas <94841588+UsernameLuxi@users.noreply.github.com>
2025-11-26 11:09:20 +0100

    eslint custom plugin for hooks in UI

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -3,12 +3,19 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import { plugin as noHooksInUI } from './eslint-rules/nohooksinui.js'
+import { globalIgnores, defineConfig } from 'eslint/config'
 
-export default tseslint.config([
+export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
+    plugins: {
+        "no-hooks-in-ui": noHooksInUI,
+    },
+    rules: {
+      "no-hooks-in-ui/no-hooks-in-ui": "error",
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
