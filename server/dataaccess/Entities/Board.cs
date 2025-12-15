a024cdf05929042bb3882a50d8b0b269daafb8c2
Casper <casfro01@easv365.dk>
2025-12-15 11:36:30 +0100

    Add user foreign key to Board and implement payment handling
    Added a [ForeignKey] attribute for UserId in the Board entity to explicitly define the relationship. Updated GameService to inject IMoneyHandler and implemented TakeMoneyFromPeople to handle payment deduction for each board when a game finishes.

--- server/dataaccess/Entities/Board.cs
+++ server/dataaccess/Entities/Board.cs
@@ -1,11 +1,14 @@
-﻿using System.Threading.Tasks.Dataflow;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Threading.Tasks.Dataflow;
 
 namespace DataAccess.Entities;
 
 public class Board
 {
     public string Id { get; set; } = null!;
-    
+
+    [ForeignKey(nameof(User))] 
+    public string UserId { get; set; }
     public User User { get; set; } = null!;
     
     public virtual ICollection<Game> Games  { get; set; } = new List<Game>();
